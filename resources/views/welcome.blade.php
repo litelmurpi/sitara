9d65908ef2bf099d6e1dfff16dab467e08133775
litelmurpi <azfabrilliant91@gmail.com>
2026-02-17 21:23:23 +0700

    chore: Remove conditional check for Vite asset loading.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
     <style>
         body {
             font-family: 'Plus Jakarta Sans', sans-serif;
